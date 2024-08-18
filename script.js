4224a3012e4442161808cddf26fc0ab4f8ec2d25
Ajeet Kumar <98801990+16ajeet@users.noreply.github.com>
2024-08-18 15:29:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const body = document.querySelector("body")
+document.addEventListener('DOMContentLoaded', function () {
+	body.textContent = "DOM load success"
+})
